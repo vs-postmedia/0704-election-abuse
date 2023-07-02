d36345b04b92c2a6d60948823e45a427f330474d
Nathan Griffiths <49567130+vs-postmedia@users.noreply.github.com>
2023-07-02 14:47:52 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// JS
+import LazyLoad from 'vanilla-lazyload';
+
+// CSS
+import './css/main.css';
+import './css/normalize.css';
+
+// IMG
+import imageList from './data/image-list';
+import placeholder from './images/placeholder.png';
+
+
+// JS
+const init = async () => {
+    const myLazyLoad = new LazyLoad();
+}
+
+init();
